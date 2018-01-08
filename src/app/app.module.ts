1c12dcbdf1c52f56a5bed86e3b0cc3f74e75797c
Axell <soad.dvm@hotmail.com>
2018-01-07 22:30:10 -0600

    Primer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AddTasksComponent } from './components/add-tasks/add-tasks.component';
 
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { TaskService } from './services/task.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    AddTasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase, 'angular-fs')
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,18 @@
+import { firebase } from "@firebase/app";
+
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAs0WBq1FzQ9QYE9awEnr7H4HP6kr7KlZQ",
+    authDomain: "angular-fs-527fb.firebaseapp.com",
+    databaseURL: "https://angular-fs-527fb.firebaseio.com",
+    projectId: "angular-fs-527fb",
+    storageBucket: "angular-fs-527fb.appspot.com",
+    messagingSenderId: "769047966475"
+  }
 };
